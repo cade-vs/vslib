36c8bed9ff789e715c590dd2d1e3f43ceed18f08
andy5995 <andy400-dev@yahoo.com>
2019-01-10 03:55:42 -0600

    Add vslib to build system
    required to complete https://github.com/cade-vs/vfu-dist/pull/5
    
    The test that was in your makefile is part of that PR, but in the "t"
    directory. running 'make check' will run the test (after make has been
    run and libs built).

--- unicon.h
+++ unicon.h
@@ -16,13 +16,36 @@
   #error "I don't know what is the target platform"
 #endif
 
+#ifdef HAVE_CONFIG_H
+  #include "config.h"
+#endif
+
 #ifdef _TARGET_UNIX_
   #ifdef _TARGET_HAVE_YASCREEN
     #define inline
     #include <yascreen.h>
     #undef inline
   #else
-    #if defined(_TARGET_LINUX_)
+    #if HAVE_CONFIG_H
+      // This will find the curses header on most systems. If it isn't found,
+      // the user will receive an error and compilation will terminate.
+      // These are defined when ../configure is run, and suggested use is
+      // mentioned in the autoconf documentation
+      // <https://www.gnu.org/software/autoconf-archive/ax_with_curses.html>
+      #if defined HAVE_NCURSESW_CURSES_H
+      #  include <ncursesw/curses.h>
+      #elif defined HAVE_NCURSESW_H
+      #  include <ncursesw.h>
+      #elif defined HAVE_NCURSES_CURSES_H
+      #  include <ncurses/curses.h>
+      #elif defined HAVE_NCURSES_H
+      #  include <ncurses.h>
+      #elif defined HAVE_CURSES_H
+      #  include <curses.h>
+      #else
+      #  error "SysV or X/Open-compatible Curses header file required"
+      #endif
+    #elif defined(_TARGET_LINUX_)
     #include <curses.h>
     #elif defined(_TARGET_NETBSD_)
     #include <ncurses.h>
